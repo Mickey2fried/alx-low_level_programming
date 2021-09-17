7b1242adda44c85893081b388665570108d8c9ea
Mickey2fried <mchljohnson113@gmail.com>
2021-09-17 13:53:02 -0700

    new commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - Adds positive numbers.
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector for values
  *
- * @argc: Number of arguments passed.
- * @argv: Array of pointers to arguments.
- *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum;
 
-	for (i = 1; i < argc; i++)
+	sum = 0;
+
+	for (i = 1; i < argc ; i++)
 	{
-		if (atoi(argv[i]))
+		for (j = 0; argv[i][j] != '\0' ; j++)
 		{
-			sum += atoi(argv[i]);
+			if (argv[i][j] < 47 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else if ((atoi(argv[i])) < '0' || (atoi(argv[i])) > '9')
-		{
-			printf("Error\n");
-			return (1);
-		}
+		sum = sum + atoi(argv[i]);
 	}
-
 	printf("%d\n", sum);
 	return (0);
 }
